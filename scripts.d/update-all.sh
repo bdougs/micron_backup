86ffafa0816ee346b4d38b5e2248090cc4d67c13
brandon smith <brandon@smith.net>
2023-07-26 14:54:41 -0700

    Autocommit from 2023-07-26 14:54:41
    Klipper on commit: d725dfd
    
    Moonraker on commit: aebcd00
    
    Mainsail version: v2.6.1

--- scripts.d/update-all.sh
+++ scripts.d/update-all.sh
@@ -5,11 +5,15 @@ git pull
 make clean KCONFIG_CONFIG=config.octopus
 make menuconfig KCONFIG_CONFIG=config.octopus
 make KCONFIG_CONFIG=config.octopus
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_310030000550314D35323820-if00 KCONFIG_CONFIG=config.octopus
-#./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f446xx_310030000550314D35323820-if00 btt-octopus-f446-v1.1
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_2B0028000850314D35323820-if00 KCONFIG_CONFIG=config.octopus
 
 make clean KCONFIG_CONFIG=config.rpi
 make menuconfig KCONFIG_CONFIG=config.rpi
 make flash KCONFIG_CONFIG=config.rpi
+
+make clean 
+make menuconfig 
+make 
+python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 235c74d5d81d
 
 sudo service klipper start
